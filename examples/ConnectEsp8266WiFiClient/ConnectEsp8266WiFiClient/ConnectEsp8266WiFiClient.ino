13b8ee548a5381975230949e1b8d52553506c0fa
kyungtae <kthan@securityplatform.co.kr>
2017-12-22 10:44:50 +0900

    initial commit

---
+++ examples/ConnectEsp8266WiFiClient/ConnectEsp8266WiFiClient/ConnectEsp8266WiFiClient.ino
@@ -0,0 +1,295 @@
+/*
+ *******************************************************************************
+ *
+ * Purpose: Example of using the Arduino MqttClient with Esp8266WiFiClient.
+ * Project URL: https://github.com/monstrenyatko/ArduinoMqtt
+ *
+ *******************************************************************************
+ * Copyright Oleg Kovalenko 2017.
+ *
+ * Distributed under the MIT License.
+ * (See accompanying file LICENSE or copy at http://opensource.org/licenses/MIT)
+ *******************************************************************************
+ */
+
+#include <Arduino.h>
+
+// Enable MqttClient logs
+#define MQTT_LOG_ENABLED 1
+// Include library
+#include <MqttClient.h>
+
+
+//ESP8266Client client;
+ESP8266Mbedtls client;
+
+//////////////////////////////
+// WiFi Network Definitions //
+//////////////////////////////
+// Replace these two character strings with the name and
+// password of your WiFi network.
+const char mySSID[] = "SecurityPlatform";
+const char myPSK[] = "57@XfX7QEaPvgVb";
+
+//////////////////
+// HTTP Strings //
+//////////////////
+const char destServer[] = "192.168.122.3";
+const int mqttPort = 8883;
+
+#define LOG_PRINTFLN(fmt, ...)	logfln(fmt, ##__VA_ARGS__)
+#define LOG_SIZE_MAX 128
+void logfln(const char *fmt, ...) {
+	char buf[LOG_SIZE_MAX];
+	va_list ap;
+	va_start(ap, fmt);
+	vsnprintf(buf, LOG_SIZE_MAX, fmt, ap);
+	va_end(ap);
+	Serial.println(buf);
+}
+
+#define HW_UART_SPEED									115200L
+#define MQTT_ID											"TEST-ID"
+const char* MQTT_TOPIC_SUB = "test/" MQTT_ID "/sub";
+const char* MQTT_TOPIC_PUB = "test/" MQTT_ID "/pub";
+
+static MqttClient *mqtt = NULL;
+//static WiFiClient network;
+//static ESP8266Client client;
+
+
+// ============== Object to supply system functions ============================
+class System: public MqttClient::System {
+public:
+
+	unsigned long millis() const {
+		return ::millis();
+	}
+
+	void yield(void) {
+		::yield();
+	}
+};
+void errorLoop(int error)
+{
+  Serial.print(F("Error: ")); Serial.println(error);
+  Serial.println(F("Looping forever."));
+  for (;;)
+    ;
+}
+
+// serialTrigger prints a message, then waits for something
+// to come in from the serial port.
+void serialTrigger(String message)
+{
+  Serial.println();
+  Serial.println(message);
+  Serial.println();
+  while (!Serial.available())
+    ;
+  while (Serial.available())
+    Serial.read();
+}
+void initializeESP8266()
+{
+  // esp8266.begin() verifies that the ESP8266 is operational
+  // and sets it up for the rest of the sketch.
+  // It returns either true or false -- indicating whether
+  // communication was successul or not.
+  // true
+  int test = esp8266.begin();
+  if (test != true)
+  {
+    Serial.println(F("Error talking to ESP8266."));
+    errorLoop(test);
+  }
+  esp8266.setMux(0);
+  Serial.println(F("ESP8266 Shield Present"));
+}
+
+void connectESP8266()
+{
+  // The ESP8266 can be set to one of three modes:
+  //  1 - ESP8266_MODE_STA - Station only
+  //  2 - ESP8266_MODE_AP - Access point only
+  //  3 - ESP8266_MODE_STAAP - Station/AP combo
+  // Use esp8266.getMode() to check which mode it's in:
+  int retVal = esp8266.getMode();
+  if (retVal != ESP8266_MODE_STA)
+  { // If it's not in station mode.
+    // Use esp8266.setMode([mode]) to set it to a specified
+    // mode.
+    retVal = esp8266.setMode(ESP8266_MODE_STA);
+    if (retVal < 0)
+    {
+      Serial.println(F("Error setting mode."));
+      errorLoop(retVal);
+    }
+  }
+  Serial.println(F("Mode set to station"));
+
+  // esp8266.status() indicates the ESP8266's WiFi connect
+  // status.
+  // A return value of 1 indicates the device is already
+  // connected. 0 indicates disconnected. (Negative values
+  // equate to communication errors.)
+  retVal = esp8266.status();
+  if (retVal <= 0)
+  {
+    Serial.print(F("Connecting to "));
+    Serial.println(mySSID);
+    // esp8266.connect([ssid], [psk]) connects the ESP8266
+    // to a network.
+    // On success the connect function returns a value >0
+    // On fail, the function will either return:
+    //  -1: TIMEOUT - The library has a set 30s timeout
+    //  -3: FAIL - Couldn't connect to network.
+    retVal = esp8266.connect(mySSID, myPSK);
+    if (retVal < 0)
+    {
+      Serial.println(F("Error connecting"));
+      errorLoop(retVal);
+    }
+  }
+}
+
+void displayConnectInfo()
+{
+  char connectedSSID[24];
+  memset(connectedSSID, 0, 24);
+  // esp8266.getAP() can be used to check which AP the
+  // ESP8266 is connected to. It returns an error code.
+  // The connected AP is returned by reference as a parameter.
+
+  int retVal=0;
+  while( retVal <= 0){
+     retVal = esp8266.getAP(connectedSSID);
+  }
+  Serial.print(F("Connected to: "));
+  Serial.println(connectedSSID);
+  
+  // esp8266.localIP returns an IPAddress variable with the
+  // ESP8266's current local IP address.
+  IPAddress myIP = esp8266.localIP();
+  Serial.print(F("My IP: ")); Serial.println(myIP);
+}
+
+// ============== Setup all objects ============================================
+void setup() {
+  // Setup hardware serial for logging
+  Serial.begin(HW_UART_SPEED);
+  while (!Serial);
+
+  // Setup WiFi network
+  WiFi.mode(WIFI_STA);
+  WiFi.hostname("ESP_" MQTT_ID);
+  WiFi.begin("ssid", "passphrase");
+  LOG_PRINTFLN("\n");
+  LOG_PRINTFLN("Connecting to WiFi");
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    LOG_PRINTFLN(".");
+  }
+  LOG_PRINTFLN("Connected to WiFi");
+  LOG_PRINTFLN("IP: %s", WiFi.localIP().toString().c_str());
+
+
+
+	// Setup MqttClient
+	MqttClient::System *mqttSystem = new System;
+	MqttClient::Logger *mqttLogger = new MqttClient::LoggerImpl<HardwareSerial>(Serial);
+	MqttClient::Network * mqttNetwork = new MqttClient::NetworkClientImpl<ESP8266Mbedtls>(client, *mqttSystem);
+  //MqttClient::Network * mqttNetwork = new MqttClient::NetworkClientImpl<ESP8266Client>(client, *mqttSystem);
+	//// Make 128 bytes send buffer
+	MqttClient::Buffer *mqttSendBuffer = new MqttClient::ArrayBuffer<128>();
+	//// Make 128 bytes receive buffer
+	MqttClient::Buffer *mqttRecvBuffer = new MqttClient::ArrayBuffer<128>();
+	//// Allow up to 2 subscriptions simultaneously
+	MqttClient::MessageHandlers *mqttMessageHandlers = new MqttClient::MessageHandlersImpl<2>();
+	//// Configure client options
+	MqttClient::Options mqttOptions;
+	////// Set command timeout to 10 seconds
+	mqttOptions.commandTimeoutMs = 10000;
+	//// Make client object
+	mqtt = new MqttClient(
+		mqttOptions, *mqttLogger, *mqttSystem, *mqttNetwork, *mqttSendBuffer,
+		*mqttRecvBuffer, *mqttMessageHandlers
+	);
+}
+// ============== Subscription callback ========================================
+void processMessage(MqttClient::MessageData& md) {
+  const MqttClient::Message& msg = md.message;
+  char payload[msg.payloadLen + 1];
+  memcpy(payload, msg.payload, msg.payloadLen);
+  payload[msg.payloadLen] = '\0';
+  LOG_PRINTFLN(
+    "Message arrived: qos %d, retained %d, dup %d, packetid %d, payload:[%s]",
+    msg.qos, msg.retained, msg.dup, msg.id, payload
+  );
+}
+// ============== Main loop ====================================================
+void loop() {
+  int r;
+	// Check connection status
+	if (!mqtt->isConnected()) {
+		// Close connection if exists
+		client.stop();
+		// Re-establish TCP connection with MQTT broker
+		LOG_PRINTFLN("Connecting");
+		client.connect(destServer, mqttPort, 1000);
+		if (!client.connected()) {
+			LOG_PRINTFLN("Can't establish the TCP connection");
+			delay(5000);
+			esp8266.reset();
+		}
+   client.setupSSL();
+      
+   int retVal = client.connectSSL();
+   if( retVal == 0){
+     Serial.println("SSL connect success!");
+   }
+   else{
+     Serial.println("SSL connect failed!");
+     return;
+   }
+		// Start new MQTT connection
+		MqttClient::ConnectResult connectResult;
+		// Connect
+		{
+			MQTTPacket_connectData options = MQTTPacket_connectData_initializer;
+			options.MQTTVersion = 4;
+			options.clientID.cstring = (char*)MQTT_ID;
+			options.cleansession = true;
+			options.keepAliveInterval = 15; // 15 seconds
+			MqttClient::Error::type rc = mqtt->connect(options, connectResult);
+			if (rc != MqttClient::Error::SUCCESS) {
+				LOG_PRINTFLN("Connection error: %i", rc);
+				return;
+			}
+		}
+		{
+			MqttClient::Error::type rc = mqtt->subscribe(
+        MQTT_TOPIC_PUB, MqttClient::QOS0, processMessage
+      );
+      if (rc != MqttClient::Error::SUCCESS) {
+        LOG_PRINTFLN("Subscribe error: %i", rc);
+        LOG_PRINTFLN("Drop connection");
+        mqtt->disconnect();
+        return;
+      }
+		}
+	} else {
+		{
+      const char* buf = "Hello World!";
+      MqttClient::Message message;
+      message.qos = MqttClient::QOS0;
+      message.retained = false;
+      message.dup = false;
+      message.payload = (void*) buf;
+      message.payloadLen = strlen(buf) + 1;
+      mqtt->publish(MQTT_TOPIC_PUB, message);
+		}
+		// Idle for 30 seconds
+		mqtt->yield(30000L);
+	}
+}
